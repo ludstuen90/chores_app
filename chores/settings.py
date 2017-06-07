ade841b2117a296e567c264f50432c711053681c
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2017-06-06 22:38:34 -0500

    chores list displayed on main page

--- chores/settings.py
+++ chores/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'chores.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['chores/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- choresapp/views.py
+++ choresapp/views.py
@@ -3,9 +3,10 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+from models import Chore
 
 # Create your views here.
 
-class HomeView(View):
-    def get(self, request):
-        return HttpResponse("Hello!")
+class HomeView(ListView):
+    model = Chore
