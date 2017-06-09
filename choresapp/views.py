cef87950993756fede045521ebf3d6e3811d3217
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2017-06-08 22:10:10 -0500

    added most recent clean times to homepage

--- choresapp/views.py
+++ choresapp/views.py
@@ -3,10 +3,24 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
-from models import Chore
+from django.views.generic import ListView, DetailView
+from models import Chore, Clean
 
 # Create your views here.
 
 class HomeView(ListView):
     model = Chore
+
+    def get_context_data(self, **kwargs):
+
+        all_cleans= {}
+        for x in Chore.objects.order_by('chore_name').all():
+            most_recent_object = Clean.objects.filter(chore_completed__id=x.id).last()
+            print(x, most_recent_object.id)
+            marshall_id = most_recent_object.id
+            all_cleans[most_recent_object.id] = most_recent_object.time
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['alldata'] = all_cleans
+        print('looking at: ', context['alldata'])
+
+        return context
