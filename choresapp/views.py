bbb44ccf7ed50340195feaefee945995d678d04a
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2017-06-11 17:15:32 -0500

    chores now appear as a form

--- choresapp/views.py
+++ choresapp/views.py
@@ -4,23 +4,28 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
+from choresapp.forms import ChoresForm
+
 from models import Chore, Clean
 
 # Create your views here.
 
-class HomeView(ListView):
+class HomeView(FormView):
+    template_name = 'chores_form.html'
     model = Chore
+    form_class = ChoresForm
 
-    def get_context_data(self, **kwargs):
-
-        all_cleans= {}
-        for x in Chore.objects.order_by('chore_name').all():
-            most_recent_object = Clean.objects.filter(chore_completed__id=x.id).last()
-            print(x, most_recent_object.id)
-            marshall_id = most_recent_object.id
-            all_cleans[most_recent_object.id] = most_recent_object.time
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['alldata'] = all_cleans
-        print('looking at: ', context['alldata'])
-
-        return context
+    # def get_context_data(self, **kwargs):
+    #
+    #     all_cleans= {}
+    #     for x in Chore.objects.order_by('chore_name').all():
+    #         most_recent_object = Clean.objects.filter(chore_completed__id=x.id).last()
+    #         print(x, most_recent_object.id)
+    #         marshall_id = most_recent_object.id
+    #         all_cleans[most_recent_object.id] = most_recent_object.time
+    #     context = super(HomeView, self).get_context_data(**kwargs)
+    #     context['alldata'] = all_cleans
+    #     print('looking at: ', context['alldata'])
+    #
+    #     return context
